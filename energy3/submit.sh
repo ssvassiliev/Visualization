f6a399dc10d0b4721e323150a28d3b1f6f283cb2
Serguei Vassiliev <serguei.vassiliev@ace-net.ca>
2019-10-10 08:50:40 -0300

    improve submission script

--- energy3/submit.sh
+++ energy3/submit.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-start=15
-end=1357
+
+echo "Usage: ./submit config_file first_frame last_frame njobs"
+echo "Example: ./submit Petit 0 238 50"
+
+if [ "$#" -ne 4 ]; then
+    echo "Illegal number of parameters"
+    exit
+fi
+
+start=$2
+end=$3
 nframes=$(($end-$start))
-ncpu=8
+ncpu=$4
+
 i=0
 job=1
 
@@ -18,7 +28,7 @@ remainder=$(($nframes % $ncpu))
 while [ $(($i+$part)) -le $nframes ]
 do
   echo "$slurm" > slurm
-  echo pvbatch Flow.py Petit $(($i+$start)), $(($i + $start + $part - 1)) >> slurm
+  echo pvbatch Flow.py $1 $(($i+$start)), $(($i + $start + $part - 1)) >> slurm
   echo sqsub slurm
   echo "submitted job" $job:
   cat slurm
@@ -26,7 +36,7 @@ do
   i=$(($i+$part))
 done
 echo "$slurm" > slurm
-echo pvbatch Flow.py Petit $(($i + $start)) $(($i + $start+ $remainder)) >> slurm
+echo pvbatch Flow.py $1 $(($i + $start)) $(($i + $start+ $remainder)) >> slurm
   echo sqsub slurm
 echo "submitted job" $job:
 cat slurm
